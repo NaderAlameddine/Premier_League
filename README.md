a660942511c7cc1012ea1bb11c335a89b82215d5
Nader Alameddine <nader11241@icloud.com>
2026-01-01 13:35:21 +0200

    Documenting Files

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 A responsive web application that displays English Premier League clubs for the 2025/26 season. The app fetches team data from an external football API and renders it dynamically using server-side templates.
 
 ## 🚀 Features
+
 - Displays Premier League clubs and team details
 - Dynamic data fetched from a football API
 - Responsive card-based layout
@@ -10,6 +11,7 @@ A responsive web application that displays English Premier League clubs for the 
 - Clean Premier League–inspired UI
 
 ## 🛠 Tech Stack
+
 - Node.js
 - Express.js
 - EJS (Server-Side Rendering)
@@ -17,19 +19,27 @@ A responsive web application that displays English Premier League clubs for the 
 - HTML5 & CSS3
 
 ## 📦 Installation
+
 1. Clone the repository
+
 ```bash
 git clone https://github.com/NaderAlameddine/epl-clubs-2025-26.git
 ```
+
 2.Install dependencies
+
 ```
 npm install
 ```
+
 3.Run the project
+
 ```
 node index.js
 ```
+
 4.Go to your browser and type :
+
 ```
 http://localhost:3000
 ```
--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 import axios from "axios";
 import bodyParser from "body-parser";
 import express from "express";
-
+// imported express
 const app = express();
+//port number used in the url of the website
 const port = 3000;
+//api sent from the creator of this third-party API
 var API_KEY = "fce192366e654abba4f6bc273e6deb2d";
 const BASE_URL = "http://api.football-data.org/v4/";
-
+//added in order to get the body for next updates if user interacted
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server is running on port  ${port} `);
 });
+//better to add a config for the header removing repetitiveness
 const config = {
   headers: {
     "X-Auth-Token": API_KEY,
   },
 };
 
+//when the user refresh and hit the main , index.ejs will appear and the teams array will be sent to it
 app.get("/", async (req, res) => {
   try {
     const result = await axios.get(BASE_URL + "competitions/PL/teams", config);
